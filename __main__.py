686aabd0fc6c297f82027228716ad275b6145188
Ninja-Koala <felix.potthast@student.uni-siegen.de>
2015-06-23 17:26:26 +0200

    Change debug code

--- __main__.py
+++ __main__.py
@@ -37,20 +37,21 @@ for line in l_data["lines"]:
 
     for c in line["objects"]:
         npc = np.array([ [[e[0], e[1]]] for e in c ])
-        cv2.drawContours(img, [npc], 0, (0, 255, 0), -1)
+        cv2.drawContours(p_img, [npc], 0, (0, 255, 0), -1)
     """
     for note in line["notes"]:
         p1 = (note[0], 0)
         p2 = (note[0], 1000)
-        cv2.line(img, p1, p2, (0, 0, 255))
+        cv2.line(p_img, p1, p2, (0, 0, 255))
         p1 = (note[1], 0)
         p2 = (note[1], 1000)
-        cv2.line(img, p1, p2, (255, 0, 0))
+        cv2.line(p_img, p1, p2, (255, 0, 0))
     """
-    for i in range(0, 1000):
-        p1 = (0, int(l_data["raster_offset"]+l_data["raster_dist"]*i))
-        p2 = (800, int(l_data["raster_offset"]+l_data["raster_dist"]*i))
-        cv2.line(img, p1, p2, (0, 0, 255))
 
-    cv2.imshow('detected role structure', img)
+    for i in range(int(l_data["height"] / l_data["raster_dist"])):
+        p1 = (0, int(l_data["raster_offset"]+l_data["raster_dist"]*i))
+        p2 = (l_data["width"], int(l_data["raster_offset"]+l_data["raster_dist"]*i))
+        cv2.line(p_img, p1, p2, (0, 0, 255))
+
+    cv2.imshow('detected role structure', p_img)
     cv2.waitKey(0)
